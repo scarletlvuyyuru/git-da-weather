8071f299651042ddfb385a66c1d1048fac87bb89
scarletlvuyyuru <135558610+scarletlvuyyuru@users.noreply.github.com>
2023-07-26 08:20:35 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+function formatDate(date) {
+  let hours = date.getHours();
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  let day = days[dayIndex];
+  return `${days[dayIndex]}, ${hours}:${minutes}`;
+}
+
+function search(city) {
+  let apiKey = "650d0d4b7a63037f35001c903db11419";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=650d0d4b7a63037f35001c903db11419&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#search-text-input").value;
+  search(city);
+}
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+}
+
+function getLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "650d0d4b7a63037f35001c903db11419";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let h1 = document.querySelector("h1");
+let currentTime = new Date();
+
+let form = document.querySelector("#searchBar");
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getPosition);
+h1.innerHTML = formatDate(currentTime);
+
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
